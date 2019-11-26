f1ca40c04c6c514a3f58a8253bd31ec6e7f6b3da
raka95 <ramin.k2013@gmail.com>
2019-11-26 12:08:26 +0330

    n

---
+++ codes/t02.py
@@ -0,0 +1,20 @@
+a=float(input("add a ra vared kon:"))
+b=float(input("add b ra vared kon:"))
+c=float(input("add c ra vared kon:"))
+if a<b+c and b<a+c and c<a+b :
+    print("mosalas mitavan sakht")
+else:
+    print("nemitavan sakht")
+
+# # Also this implementation in correct :
+# a=float(input("add a ra vared kon:"))
+# b=float(input("add b ra vared kon:"))
+# c=float(input("add c ra vared kon:"))
+# if a>=b+c:
+#     print("No")
+# elif b>=a+c:
+#     print("No")
+# elif c>a+b:
+#     print("No")
+# else:
+#     print("Yes")
